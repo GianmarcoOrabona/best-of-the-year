9f3a31078e72b0f98c41cb97b8a8334181bb66b1
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-04 14:53:17 +0100

    add song class

---
+++ src/main/java/org/learning/bestoftheyear/model/Song.java
@@ -0,0 +1,55 @@
+package org.learning.bestoftheyear.model;
+
+public class Song {
+    // ATTRIBUTI
+
+    private int id;
+    private String title;
+    private String author;
+
+    // COSTRUTTORI
+
+    public Song(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+
+    // GETTER E SETTER
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // METODI
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
