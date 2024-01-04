14ce4fbe6558421528f3c96c8acea6af0db1810f
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-04 14:37:34 +0100

    step 1 done

--- src/main/java/org/learning/bestoftheyear/controller/IndexController.java
+++ src/main/java/org/learning/bestoftheyear/controller/IndexController.java
@@ -1,9 +1,23 @@
 package org.learning.bestoftheyear.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class IndexController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("year_recap")
+    public String getDate(Model model) {
+        model.addAttribute("year", 2023);
+        model.addAttribute("name", "Gianmarco");
+        return "year_recap";
+    }
 }
